b1d9cf481d77eed863cfe43e3836883af51c6950
Unity Chaos <86889904+UnityChaos@users.noreply.github.com>
2024-10-16 21:05:59 -0700

    add atom and tia support (#1)

--- src/IBC.py
+++ src/IBC.py
@@ -72,125 +72,123 @@ def ibc_transfer(src, dst, amount, token):
 
 
 def withdraw(chain, amount, token):
-    match chain:
-        case "Noble":
-            print("noble withdraw")
-            denom = get_denom("Penumbra", token)
-            print(f"{amount}{denom}")
-            cmd = [
-                "pcli",
-                "tx",
-                "withdraw",
-                "--to",
-                get_sdk_address("Noble"),
-                "--channel",
-                get_channel("Penumbra", "Noble"),
-                f"{amount}{denom}",
-                "--source",
-                Args.Penumbra_Account,
-                "--use-compat-address",
-            ]
-            print(cmd)
-            r = subprocess.run(cmd, capture_output=True, text=True)
-            print("errors: ", r.stderr)
-            print("output: ", r.stdout)
-            print(
-                "sleeping 90 seconds to avoid IBC spam failures"
-            )  # FIXME check ack on receiver chain instead
-            time.sleep(90)
-            return r
-        case "Osmosis":
-            denom = get_denom("Penumbra", token)
-            print(f"{amount}{denom}")
-            cmd = [
-                "pcli",
-                "tx",
-                "withdraw",
-                "--to",
-                get_osmosis_address(),
-                "--channel",
-                get_channel("Penumbra", "Osmosis"),
-                f"{amount}{denom}",
-                "--source",
-                Args.Penumbra_Account,
-            ]
-            print(cmd)
+    if chain == "Noble":
+        print("noble withdraw")
+        denom = get_denom("Penumbra", token)
+        print(f"{amount}{denom}")
+        cmd = [
+            "pcli",
+            "tx",
+            "withdraw",
+            "--to",
+            get_sdk_address("Noble"),
+            "--channel",
+            get_channel("Penumbra", "Noble"),
+            f"{amount}{denom}",
+            "--source",
+            Args.Penumbra_Account,
+            "--use-compat-address",
+        ]
+        print(cmd)
+        r = subprocess.run(cmd, capture_output=True, text=True)
+        print("errors: ", r.stderr)
+        print("output: ", r.stdout)
+        print(
+            "sleeping 90 seconds to avoid IBC spam failures"
+        )  # FIXME check ack on receiver chain instead
+        time.sleep(90)
+        return r
+        
+    denom = get_denom("Penumbra", token)
+    print(f"{amount}{denom}")
+    cmd = [
+        "pcli",
+        "tx",
+        "withdraw",
+        "--to",
+        get_sdk_address(chain),
+        "--channel",
+        get_channel("Penumbra", chain),
+        f"{amount}{denom}",
+        "--source",
+        Args.Penumbra_Account,
+    ]
+    print(cmd)
 
-            r = subprocess.run(cmd, capture_output=True, text=True)
-            print("errors: ", r.stderr)
-            print("output: ", r.stdout)
-            print(
-                "sleeping 90 seconds to avoid IBC spam failures"
-            )  # FIXME check ack on receiver chain instead
-            time.sleep(90)
-            return r
+    r = subprocess.run(cmd, capture_output=True, text=True)
+    print("errors: ", r.stderr)
+    print("output: ", r.stdout)
+    print(
+        "sleeping 90 seconds to avoid IBC spam failures"
+    )  # FIXME check ack on receiver chain instead
+    time.sleep(90)
+    return r
 
 
 def deposit(chain, amount, token):
-    match chain:
-        case "Noble":
-            print("noble deposit")
-            cmd = [
-                get_binary(chain),
-                "tx",
-                "ibc-transfer",
-                "transfer",
-                "transfer",
-                f"channel-{get_channel(chain, 'Penumbra')}",
-                get_penumbra_deposit_address(chain == "Noble"),
-                f"{amount}{get_denom(chain, token)}",
-                f"--node={get_rpc(chain)}",
-                "--keyring-backend=test",
-                f"--chain-id={get_chain_id(chain)}",
-                "--gas=auto",
-                "--gas-adjustment=1.5",
-                f"--gas-prices={get_gas_price(chain)}",
-                f"--from={get_wallet(chain)}",
-                "--output=json",
-                "--packet-timeout-height=1-1800",
-                "-y",
-            ]
-            print(cmd)
-            r = subprocess.run(cmd, capture_output=True, text=True)
-            print("errors: ", r.stderr)
-            print("output: ", r.stdout)
-            j = json.loads(r.stdout)
-            hash = j["txhash"]
-            await_sdk_tx(chain, hash, 30)
-            print(
-                "sleeping 90 seconds to avoid IBC spam failures"
-            )  # FIXME check ack on receiver chain instead
-            time.sleep(90)
-            return hash
-        case "Osmosis":
-            cmd = [
-                get_binary(chain),
-                "tx",
-                "ibc-transfer",
-                "transfer",
-                "transfer",
-                f"channel-{get_channel(chain, 'Penumbra')}",
-                get_penumbra_deposit_address(chain == "Noble"),
-                f"{amount}{get_denom(chain, token)}",
-                f"--node={get_rpc(chain)}",
-                "--keyring-backend=test",
-                f"--chain-id={get_chain_id(chain)}",
-                "--gas=auto",
-                "--gas-adjustment=1.5",
-                f"--gas-prices={get_gas_price(chain)}",
-                f"--from={get_wallet(chain)}",
-                "--output=json",
-                "-y",
-            ]
-            print(cmd)
-            r = subprocess.run(cmd, capture_output=True, text=True)
-            print("errors: ", r.stderr)
-            print("output: ", r.stdout)
-            j = json.loads(r.stdout)
-            hash = j["txhash"]
-            await_osmosis_tx(hash, 30)
-            print(
-                "sleeping 90 seconds to avoid IBC spam failures"
-            )  # FIXME check ack on receiver chain instead
-            time.sleep(90)
-            return hash
+    if chain == "Noble":
+        print("noble deposit")
+        cmd = [
+            get_binary(chain),
+            "tx",
+            "ibc-transfer",
+            "transfer",
+            "transfer",
+            f"channel-{get_channel(chain, 'Penumbra')}",
+            get_penumbra_deposit_address(True),
+            f"{amount}{get_denom(chain, token)}",
+            f"--node={get_rpc(chain)}",
+            "--keyring-backend=test",
+            f"--chain-id={get_chain_id(chain)}",
+            "--gas=auto",
+            "--gas-adjustment=1.5",
+            f"--gas-prices={get_gas_price(chain)}",
+            f"--from={get_wallet(chain)}",
+            "--output=json",
+            "--packet-timeout-height=1-1800",
+            "-y",
+        ]
+        print(cmd)
+        r = subprocess.run(cmd, capture_output=True, text=True)
+        print("errors: ", r.stderr)
+        print("output: ", r.stdout)
+        j = json.loads(r.stdout)
+        hash = j["txhash"]
+        await_sdk_tx(chain, hash, 30)
+        print(
+            "sleeping 90 seconds to avoid IBC spam failures"
+        )  # FIXME check ack on receiver chain instead
+        time.sleep(90)
+        return hash
+    
+    cmd = [
+        get_binary(chain),
+        "tx",
+        "ibc-transfer",
+        "transfer",
+        "transfer",
+        f"channel-{get_channel(chain, 'Penumbra')}",
+        get_penumbra_deposit_address(False),
+        f"{amount}{get_denom(chain, token)}",
+        f"--node={get_rpc(chain)}",
+        "--keyring-backend=test",
+        f"--chain-id={get_chain_id(chain)}",
+        "--gas=auto",
+        "--gas-adjustment=1.5",
+        f"--gas-prices={get_gas_price(chain)}",
+        f"--from={get_wallet(chain)}",
+        "--output=json",
+        "-y",
+    ]
+    print(cmd)
+    r = subprocess.run(cmd, capture_output=True, text=True)
+    print("errors: ", r.stderr)
+    print("output: ", r.stdout)
+    j = json.loads(r.stdout)
+    hash = j["txhash"]
+    await_osmosis_tx(hash, 30)
+    print(
+        "sleeping 90 seconds to avoid IBC spam failures"
+    )  # FIXME check ack on receiver chain instead
+    time.sleep(90)
+    return hash
